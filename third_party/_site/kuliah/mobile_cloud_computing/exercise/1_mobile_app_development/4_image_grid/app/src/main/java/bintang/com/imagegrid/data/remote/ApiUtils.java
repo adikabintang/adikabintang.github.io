9ad8b8a696ce41924d5c9e7b80e51ae967320b89
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2019-06-09 01:46:58 +0300

    update

--- third_party/_site/kuliah/mobile_cloud_computing/exercise/1_mobile_app_development/4_image_grid/app/src/main/java/bintang/com/imagegrid/data/remote/ApiUtils.java
+++
@@ -1,7 +0,0 @@
-package bintang.com.imagegrid.data.remote;
-
-public class ApiUtils {
-    public static JsonAccessService getJsonAccessService() {
-        return RetrofitClient.getClient("https://api.myjson.com/").create(JsonAccessService.class);
-    }
-}
--- third_party/_site/kuliah/mobile_cloud_computing/exercise/2_backend_nodejs/4/exercise/README.md
+++
@@ -1,1 +0,0 @@
-https://medium.freecodecamp.org/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52
